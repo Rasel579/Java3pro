02f4592c1d5c626a40050c7d616077bad3f56949
Rasel579 <Rasel579@gmail.com>
2021-02-04 02:21:09 +0500

    finished hw for lesson1 about Generics

---
+++ src/main/java/lesson1_generics/Fruit.java
@@ -0,0 +1,16 @@
+package lesson1_generics;
+
+public class Fruit {
+    private float weight;
+
+    public Fruit() {
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
